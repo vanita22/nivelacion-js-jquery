23f08822ce847ac39cbe79d17f76157a0789f5ec
vanita22 <vanitayrafita2004@gmail.com>
2017-07-10 21:44:31 -0300

    subiendo nuevamente el nivel 3 que no se agregaron los cambios correctamente donde hago el recorrido con un forEach en la funcion renderHighlightedRecipes para poder validar con un if si el highlighted es igual al booleano true imprimir por pantalla el mensaje contenido en la función renderRecipe

--- js/script-index.js
+++ js/script-index.js
@@ -21,7 +21,9 @@ function renderHighlightedRecipes(recipesArray) {
 
 	/*agregado de Inés Vanessa Pérez*/
 	recipesArray.forEach(function(el){		
-			console.log(el.highlighted);		
+			if(el.highlighted === true){
+				console.log(renderRecipe());
+			}		
 	});	
 }
 
@@ -35,6 +37,8 @@ renderHighlightedRecipes(recipesArray);
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+	/*agregado de Inés Vanessa Pérez Chávez*/
+	$(".list-recipes").append("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'> TITULO DE LA RECETA (ATRIBUTO 'title' ) </span><span class='metadata-recipe'><span class='author-recipe'> NOMBRE DEL AUTO DE LA RECETA (ATRIBUTO 'source.name') </span><span class='bookmarks-recipe'><span class='icon-bookmark'></span></span></span></span><img src='URL DE LA IMAGEN' /></a>");
 }
 
 
