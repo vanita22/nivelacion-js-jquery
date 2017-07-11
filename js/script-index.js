d73bd7517556d660cf1c94d395c9f0b4e7698647
vanita22 <vanitayrafita2004@gmail.com>
2017-07-10 22:50:28 -0300

    agregue todo el formato que salía en el template-recipe.html y lo guarde adentro del append usando jquery para agregarlo a la clase list-recipes, todo esto va dentro de una función, usé el forEach para hacer un nuevo recorrido y llegar a que me imprima los titulos, nombres e imagenes

--- js/script-index.js
+++ js/script-index.js
@@ -36,10 +36,13 @@ renderHighlightedRecipes(recipesArray);
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
 	/*agregado de Inés Vanessa Pérez Chávez*/
-	$(".list-recipes").append("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'> TITULO DE LA RECETA (ATRIBUTO 'title' ) </span><span class='metadata-recipe'><span class='author-recipe'> NOMBRE DEL AUTO DE LA RECETA (ATRIBUTO 'source.name') </span><span class='bookmarks-recipe'><span class='icon-bookmark'></span></span></span></span><img src='URL DE LA IMAGEN' /></a>");
+	recipesArray.forEach(function(el){	
+		$(".list-recipes").append("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'>" + el.title + " </span><span class='metadata-recipe'><span class='author-recipe'>" + el.source.name + "</span><span class='bookmarks-recipe'><span class='icon-bookmark'></span></span></span></span><img src='" + el.source.url + "'/></a>");
+	});
+	console.log('Voy a pintar la receta: ', recipe);
 }
+renderRecipe();
 
 
 
